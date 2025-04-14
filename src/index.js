f5dd76e07a2ae07ca8cdc0985550a55c16fd576f
rowhit <rk301855@gmail.com>
2025-04-14 22:36:57 +0530

    add register controller and fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
 connectDB()
 .then(()=>{
-    AudioParamMap.listen(process.env.PORT || 8080, () => {
+    app.listen(process.env.PORT || 8080, () => {
         console.log(`server is running at PORT : ${process.env.PORT}`);
     })
 })
