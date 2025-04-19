1b190ae8ccce8af8bc95c5e09737489b7a715ef0
unknown <rk301855@gmail.com>
2025-04-19 19:17:25 +0530

    fixed the bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+})
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { ApiError } from "./ApiError.js";
 
 
     // Configuration
@@ -11,7 +16,9 @@ import fs from "fs";
 
     const uploadOnCloudinary = async (localFilePath) => {
         try{
-            if(!localFilePath) return null
+            if(!localFilePath) {
+                throw new ApiError(400, "No file path provided");
+            }
             //upload the file on cloudinary
 
             const response = await cloudinary.uploader.upload(
@@ -19,12 +26,15 @@ import fs from "fs";
                     resource_type: "auto"
                 }
             )
+            console.log("File uploaded to Cloudinary:", response.secure_url);
+            
             //file has been uploaded succesfully
             console.log("file is uploaded on cloudniary",
                 response.url
             );
             return response;
         }catch(error){
+            console.log("failed to upload cus:", error)
             throw new ApiError(400, "Failed to upload avatar to Cloudinary");
             fs.unlinkSync(localFilePath) // remove the locally saved  temporary file as the as the upload operation got failed
             return null;
